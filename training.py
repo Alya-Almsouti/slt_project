ba2a4fe38c73f726bfd404b57946e957d016c1e1
Alya-Almsouti <alyaalmsouti@gmail.com>
2025-03-16 06:01:52 +0400

    Your commit message

--- training.py
+++ training.py
@@ -104,55 +104,45 @@ def main(args):
 
 def train_one_epoch(args, model, data_loader, optimizer, epoch):
     model.train()
-
     metric_logger = utils.MetricLogger(delimiter="  ")
     metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    header = 'Epoch: [{}/{}]'.format(epoch, args.epochs)
+    header = f'Epoch: [{epoch}/{args.epochs}]'
     print_freq = 10
     optimizer.zero_grad()
 
-    target_dtype = None
-    if model.bfloat16_enabled():
-        target_dtype = torch.bfloat16
-
     for step, (src_input, tgt_input) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-        if step %10 == 0:
-            # print(torch.cuda.memory_summary())
-            print(f"Memory allocated: {torch.cuda.memory_allocated() / 1e9} GB")
-        if (step + 1) % args.quick_break == 0:
-            if args.output_dir:
-                output_dir = Path(args.output_dir)
-                checkpoint_paths = [output_dir / f'checkpoint.pth']
-                for checkpoint_path in checkpoint_paths:
-                    utils.save_on_master({
-                        'model': get_requires_grad_dict(model),
-                    }, checkpoint_path)
 
-        if target_dtype != None:
-            for key in src_input.keys():
-                if isinstance(src_input[key], torch.Tensor):
-                    src_input[key] = src_input[key].to(target_dtype).cuda()
+        # Convert input tensors to bfloat16 before passing them to model
+        for key in src_input.keys():
+            if isinstance(src_input[key], torch.Tensor):
+                src_input[key] = src_input[key].to(dtype=torch.bfloat16).cuda()
 
-        stack_out = model(src_input, tgt_input)
-        
+        with torch.autocast(device_type='cuda', dtype=torch.bfloat16):  
+            stack_out = model(src_input, tgt_input)  
+
         total_loss = stack_out['loss']
-        model.backward(total_loss)
-        model.step()
+        total_loss.backward()
+
+        if (step + 1) % args.gradient_accumulation_steps == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+
+        if step % 10 == 0:
+            print(f"Memory allocated: {torch.cuda.memory_allocated() / 1e9} GB")
 
         loss_value = total_loss.item()
         if not math.isfinite(loss_value):
             print("Loss is {}, stopping training".format(loss_value))
             sys.exit(1)
-            
+
         metric_logger.update(loss=loss_value)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-        break
 
-    # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
 
-    return  {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
 
 
 def evaluate(args, data_loader, model):
@@ -161,66 +151,64 @@ def evaluate(args, data_loader, model):
     metric_logger = utils.MetricLogger(delimiter="  ")
     header = 'Test:'
 
-    target_dtype = None
-    if model.bfloat16_enabled():
-        target_dtype = torch.bfloat16
-        
     with torch.no_grad():
         tgt_pres = []
         tgt_refs = []
- 
+
         for step, (src_input, tgt_input) in enumerate(metric_logger.log_every(data_loader, 10, header)):
-            if target_dtype != None:
-                for key in src_input.keys():
-                    if isinstance(src_input[key], torch.Tensor):
-                        src_input[key] = src_input[key].to(target_dtype).cuda()
-            
-            stack_out = model(src_input, tgt_input)
-            total_loss = stack_out['loss']
-            metric_logger.update(loss=total_loss.item())
-        
-            output = model.generate(stack_out, 
-                                    max_new_tokens=100, 
-                                    num_beams = 4,
-                        )
+
+            # Ensure src_input is converted to bfloat16
+            for key in src_input.keys():
+                if isinstance(src_input[key], torch.Tensor):
+                    src_input[key] = src_input[key].to(dtype=torch.bfloat16).cuda()
+
+            with torch.autocast(device_type='cuda', dtype=torch.bfloat16):
+                stack_out = model(src_input, tgt_input)
+                total_loss = stack_out['loss']
+                metric_logger.update(loss=total_loss.item())
+
+                # Ensure model.generate() runs in autocast mode
+                output = model.generate(stack_out, 
+                                        max_new_tokens=100, 
+                                        num_beams=4)
 
             for i in range(len(output)):
                 tgt_pres.append(output[i])
                 tgt_refs.append(tgt_input['gt_sentence'][i])
-            break
 
     tokenizer = model.mt5_tokenizer
     padding_value = tokenizer.eos_token_id
-    
-    pad_tensor = torch.ones(150-len(tgt_pres[0])).cuda() * padding_value
-    tgt_pres[0] = torch.cat((tgt_pres[0],pad_tensor.long()),dim = 0)
 
-    tgt_pres = pad_sequence(tgt_pres,batch_first=True,padding_value=padding_value)
+    pad_tensor = torch.ones(150 - len(tgt_pres[0]), dtype=torch.long).cuda() * padding_value
+    tgt_pres[0] = torch.cat((tgt_pres[0], pad_tensor), dim=0)
+
+    tgt_pres = pad_sequence(tgt_pres, batch_first=True, padding_value=padding_value)
     tgt_pres = tokenizer.batch_decode(tgt_pres, skip_special_tokens=True)
-            
-    if args.dataset == 'CSL_News' or args.dataset == 'Open_ASL':
-        tgt_pres = [' '.join(list(r.replace(" ",'').replace("\n",''))) for r in tgt_pres]
-        tgt_refs = [' '.join(list(r.replace("，", ',').replace("？","?").replace(" ",''))) for r in tgt_refs]
+
+    if args.dataset in ['CSL_News', 'Open_ASL']:
+        tgt_pres = [' '.join(list(r.replace(" ", '').replace("\n", ''))) for r in tgt_pres]
+        tgt_refs = [' '.join(list(r.replace("，", ',').replace("？", "?").replace(" ", ''))) for r in tgt_refs]
 
     bleu_dict, rouge_score = translation_performance(tgt_refs, tgt_pres)
-    for k,v in bleu_dict.items():
+    for k, v in bleu_dict.items():
         metric_logger.meters[k].update(v)
     metric_logger.meters['rouge'].update(rouge_score)
 
-    # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print('* BLEU-4 {top1.global_avg:.3f} loss {losses.global_avg:.3f}'
           .format(top1=metric_logger.bleu4, losses=metric_logger.loss))
-    
+
     if utils.is_main_process() and utils.get_world_size() == 1 and args.eval:
-        with open(args.output_dir+'/tmp_pres.txt','w') as f:
+        with open(args.output_dir + '/tmp_pres.txt', 'w') as f:
             for i in range(len(tgt_pres)):
-                f.write(tgt_pres[i]+'\n')
-        with open(args.output_dir+'/tmp_refs.txt','w') as f:
+                f.write(tgt_pres[i] + '\n')
+        with open(args.output_dir + '/tmp_refs.txt', 'w') as f:
             for i in range(len(tgt_refs)):
-                f.write(tgt_refs[i]+'\n')
+                f.write(tgt_refs[i] + '\n')
 
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
 
 if __name__ == '__main__':
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
